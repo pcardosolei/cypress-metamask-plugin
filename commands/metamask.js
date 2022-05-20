5ad7ad5993ac824f41a0bfda9dfd02b17061bb95
Paulo Cardoso <pcardoso.lei@gmail.com>
2022-05-20 09:57:36 +0100

    adding some changes on readme and removing the setupMetamask

--- commands/metamask.js
+++ commands/metamask.js
@@ -35,7 +35,6 @@ let walletAddress;
 let switchBackToCypressWindow;
 
 let accessAccepted = false;
-let setupMetamaskLogin = false;
 
 module.exports = {
   walletAddress: () => {
@@ -628,7 +627,6 @@ module.exports = {
     return walletAddress;
   },
   initialSetup: async ({ secretWordsOrPrivateKey, network, password }) => {
-    if(setupMetamaskLogin) { return true; }
     const isCustomNetwork =
       (process.env.NETWORK_NAME &&
         process.env.RPC_URL &&
@@ -680,13 +678,11 @@ module.exports = {
 
       walletAddress = await module.exports.getWalletAddress();
       await puppeteer.switchToCypressWindow();
-      setupMetamaskLogin = true;
       return true;
     } else {
       await module.exports.unlock(password);
       walletAddress = await module.exports.getWalletAddress();
       await puppeteer.switchToCypressWindow();
-      setupMetamaskLogin = true;
       return true;
     }
   },
